9b9876440ebd288faeea5d0915e1f974b6c9269e
Adamko :) <adam.hrac@gmail.com>
2020-11-01 19:12:51 +0100

    stlpcovy vypis

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ mapa = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
 
+def vypis(sachovnica, riadky, stlpce):
+    for i in range(riadky):
+        for j in range(stlpce):
+            print(sachovnica[i][j], end=" ")
+        print("\n")
+
+
 def posun_fitness(population, riadky, stlpce):
     new_mapa = mapa.copy()
     for i in population:
@@ -21,7 +28,13 @@ def posun_fitness(population, riadky, stlpce):
                     new_mapa[i][j] = i
                 else:
                     break
-    print(new_mapa)
+        if i > 9:
+            for j in range(0, riadky):
+                if new_mapa[9-j][i-10] == 0:
+                    new_mapa[9-j][i-10] = i
+                else:
+                    break
+    vypis(new_mapa, riadky, stlpce)
 
 
 if __name__ == "__main__":

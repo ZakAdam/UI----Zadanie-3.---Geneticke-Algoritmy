4bb70be001c3de753c23b0f4fa74a6928cf99bc6
Adamko :) <adam.hrac@gmail.com>
2020-11-12 00:33:00 +0100

    predspanim commit, dorobena fitness funkcia?

--- trieda_pokus.py
+++ trieda_pokus.py
@@ -1,6 +1,7 @@
 import random
 import gen_class
 from tabulate import tabulate
+from copy import copy, deepcopy
 
 mapa = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0],
@@ -14,7 +15,7 @@ mapa = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
 
-def naraz(gen, mapa):
+def naraz(gen, mapa, riadky, stlpce):
     smer = gen.get_smer()
     if smer == "Up" or smer == "Down":
         if smer == "Up":
@@ -82,14 +83,14 @@ def posun(gen):
         gen.set_x_posun(-1)
 
 
-def check(gen):
+def check(gen, riadky, stlpce):
     if 0 <= gen.x_posun < stlpce and 0 <= gen.y_posun < riadky:
         return True
     return False
 
 
-def hrabanie(population):
-    new_mapa = mapa
+def hrabanie(population, riadky, stlpce, fitness_zoznam):
+    new_mapa = deepcopy(mapa)
     koniec = False
     for i in population:
         gen = gen_class.Gene(i, riadky, stlpce)
@@ -97,12 +98,11 @@ def hrabanie(population):
         if new_mapa[suradnice[1]][suradnice[0]] != 0:
             continue
         new_mapa[suradnice[1]][suradnice[0]] = i
-        print(i)
         posun(gen)
         suradnice = gen.get_posun()
-        while check(gen):
+        while check(gen, riadky, stlpce):
             if new_mapa[suradnice[1]][suradnice[0]] != 0:
-                value = naraz(gen, new_mapa)
+                value = naraz(gen, new_mapa, riadky, stlpce)
                 if value == 3:
                     print("Tento gen nenasiel cestu von :(  --> " + str(i))
                     koniec = True
@@ -124,19 +124,44 @@ def hrabanie(population):
             if new_mapa[i][j] != 0:
                 fitness += 1
     print("Fitness danho jedinca je: " + str(fitness))
+    if fitness == riadky * stlpce:
+        print(tabulate(new_mapa))
+        print("NASIEL SI ODPOVED!!!")
+        exit(0)
+    fitness_zoznam.append(fitness)
     print(tabulate(new_mapa))
 
 
-if __name__ == "__main__":
+def main():
     riadky = len(mapa)
     stlpce = len(mapa[0])
+    pocet_jedincov = 20
+    fitness_zoznam = []
     print(riadky)
     print(stlpce)
     number_of_genes = riadky + stlpce  # TODO add aj geny pre kamene
     genes = []
-    for i in range(1, (riadky + stlpce) * 2 + 1):
-        genes.append(i)
-    population = random.sample(genes, number_of_genes)
-    print(population)
-    hrabanie(population)
+    for j in range(0, pocet_jedincov):
+        for i in range(1, number_of_genes * 2 + 1):
+            genes.append(i)
+        population = random.sample(genes, number_of_genes)
+        print(population)
+        hrabanie(population, riadky, stlpce, fitness_zoznam)
+
+    max = fitness_zoznam[0]
+    min = fitness_zoznam[0]
+    sum = 0
+    for value in fitness_zoznam:
+        if value > max:
+            max = value
+        elif value < min:
+            min = value
+        sum += value
+
+    print(fitness_zoznam)
+    print("\nMax. prvok: " + str(max) + " Min. prvok " + str(min) + " primer " + str(sum/pocet_jedincov))
+
+
+if __name__ == "__main__":
+    main()
 

1782f88aad9e0f10cac03615e33f75c87234aa92
Adamko :) <adam.hrac@gmail.com>
2020-11-12 15:29:23 +0100

    priadnie objektoveho hrabanie, nefunkcne este

--- gen_class.py
+++ gen_class.py
@@ -1,24 +1,28 @@
-class Gene:                                                     #TODO pozor na ten 0ty riadok pre Y a X
+class Gene:                                                     # TODO pozor na ten 0ty riadok pre Y a X
     def __init__(self, start, riadky, stlpce):
         self.start = start
         if riadky < start <= stlpce + riadky:
             self.smer = "Up"
+            self.povodny_smer = "Up"
             self.x = start - riadky - 1
             self.y = riadky - 1
         elif riadky + stlpce + riadky < start <= riadky + stlpce + riadky + stlpce:
             self.smer = "Down"
+            self.povodny_smer = "Down"
             self.x = stlpce - (start - riadky*2 - stlpce)
             self.y = 0
         elif start <= riadky:
             self.smer = "Right"
+            self.povodny_smer = "Right"
             self.x = 0
             self.y = start - 1
         elif riadky + stlpce < start <= riadky + stlpce + riadky:
             self.smer = "Left"
+            self.povodny_smer = "Left"
             self.x = stlpce - 1
             self.y = riadky - (start - riadky - stlpce)
         else:
-            self.smer = "Noneeeee"
+            self.smer = "Noneeeee"                              # TODO, asi do preč ne?
             self.x = 0
             self.y = 0
         self.x_posun = self.x
--- trieda_pokus.py
+++ trieda_pokus.py
@@ -1,7 +1,8 @@
 import random
 import gen_class
 from tabulate import tabulate
-from copy import copy, deepcopy
+from copy import deepcopy
+import random
 
 mapa = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0],
@@ -13,6 +14,28 @@ mapa = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+
+def turnaj(zoznam_fitness):
+    first = random.randint(0, len(zoznam_fitness) - 1)
+    second = random.randint(0, len(zoznam_fitness) - 1)
+
+    if zoznam_fitness[first] > zoznam_fitness[second]:
+        print("Prvy je: " + str(zoznam_fitness[first]) + " Druhy je: " + str(zoznam_fitness[second]) + " vrateny je prvy")
+        return first
+    else:
+        print("Prvy je: " + str(zoznam_fitness[first]) + " Druhy je: " + str(zoznam_fitness[second]) + " vrateny je druhyyyyyyy")
+        return second
+
+
+def krizenie(prvy_rodic, druhy_rodic):
+    prve_pomocne = druhy_rodic[len(druhy_rodic)//2:]
+    druhe_pomocne = prvy_rodic[len(prvy_rodic)//2:]
+
+    prve_dieta = prvy_rodic[:len(prvy_rodic) // 2] + prve_pomocne
+    druhe_dieta = druhy_rodic[:len(druhy_rodic) // 2] + druhe_pomocne
+
+    return prve_dieta, druhe_dieta
 
 
 def naraz(gen, mapa, riadky, stlpce):
@@ -28,6 +51,8 @@ def naraz(gen, mapa, riadky, stlpce):
                 gen.set_smer("Left")
                 gen.set_y_posun(1)
                 return 1
+            if (suradnice[1] - 1) == riadky - 1:
+                return 2
 
         else:
             suradnice = gen.get_posun()
@@ -39,6 +64,8 @@ def naraz(gen, mapa, riadky, stlpce):
                 gen.set_smer("Right")
                 gen.set_y_posun(-1)
                 return 1
+            if (suradnice[1] - 1) == 0:
+                return 2
         if suradnice[0] == (stlpce - 1) or suradnice[0] == 0:  # pripad, že je to bod na hranici
             return 2
 
@@ -54,6 +81,8 @@ def naraz(gen, mapa, riadky, stlpce):
                     gen.set_smer("Down")
                     gen.set_x_posun(-1)
                     return 1
+            if suradnice[0] == 0:
+                return 2
 
         else:
             suradnice = gen.get_posun()
@@ -66,7 +95,9 @@ def naraz(gen, mapa, riadky, stlpce):
                     gen.set_smer("Up")
                     gen.set_x_posun(1)
                     return 1
-        if suradnice[1] == (riadky - 1) or suradnice[1] == 0:            # pripad, že je to bod na hranici
+            if suradnice[0] == stlpce - 1:                      # hranicny stav
+                return 2
+        if suradnice[1] == (riadky - 1) or suradnice[1] == 0:   # pripad, že je to bod na hranici
             return 2
 
     return 3
@@ -90,6 +121,7 @@ def check(gen, riadky, stlpce):
 
 
 def hrabanie(population, riadky, stlpce, fitness_zoznam):
+    list_objektov = []
     new_mapa = deepcopy(mapa)
     koniec = False
     for i in population:
@@ -117,6 +149,7 @@ def hrabanie(population, riadky, stlpce, fitness_zoznam):
             suradnice = gen.get_posun()
         if koniec:
             break
+        list_objektov.append(gen)
 
     fitness = 0
     for i in range(0, riadky):
@@ -130,23 +163,78 @@ def hrabanie(population, riadky, stlpce, fitness_zoznam):
         exit(0)
     fitness_zoznam.append(fitness)
     print(tabulate(new_mapa))
+    print(list_objektov)
+    return list_objektov
+
+
+def hrabanie_2(population, riadky, stlpce, fitness_zoznam):
+    list_objektov = []
+    new_mapa = deepcopy(mapa)
+    koniec = False
+    for i in range(0, len(population)):
+        gen = population[i]
+        gen.set_smer(gen.povodny_smer)
+        gen.set_x_posun(gen.x)
+        gen.set_y_posun(gen.y)
+        suradnice = gen.get_suradnice()
+        if new_mapa[suradnice[1]][suradnice[0]] != 0:
+            continue
+        new_mapa[suradnice[1]][suradnice[0]] = gen.start
+        posun(gen)
+        suradnice = gen.get_posun()
+        while check(gen, riadky, stlpce):
+            if new_mapa[suradnice[1]][suradnice[0]] != 0:
+                value = naraz(gen, new_mapa, riadky, stlpce)
+                if value == 3:
+                    print("Tento gen nenasiel cestu von :(  --> " + str(gen.start))
+                    koniec = True
+                    break
+                elif value == 1:
+                    posun(gen)
+                    suradnice = gen.get_posun()
+                else:
+                    break
+            new_mapa[suradnice[1]][suradnice[0]] = gen.start
+            posun(gen)
+            suradnice = gen.get_posun()
+        if koniec:
+            break
+        list_objektov.append(gen)
+
+    fitness = 0
+    for i in range(0, riadky):
+        for j in range(0, stlpce):
+            if new_mapa[i][j] != 0:
+                fitness += 1
+    print("Fitness danho jedinca je: " + str(fitness))
+    if fitness == riadky * stlpce:
+        print(tabulate(new_mapa))
+        print("NASIEL SI ODPOVED!!!")
+        exit(0)
+    fitness_zoznam.append(fitness)
+    print(tabulate(new_mapa))
+    print(list_objektov)
+    return list_objektov
 
 
 def main():
     riadky = len(mapa)
     stlpce = len(mapa[0])
     pocet_jedincov = 20
+    pocet_generacii = 100
     fitness_zoznam = []
+    zoznam_objektov = {}
     print(riadky)
     print(stlpce)
     number_of_genes = riadky + stlpce  # TODO add aj geny pre kamene
     genes = []
+    for i in range(1, number_of_genes * 2 + 1):
+        genes.append(i)
     for j in range(0, pocet_jedincov):
-        for i in range(1, number_of_genes * 2 + 1):
-            genes.append(i)
         population = random.sample(genes, number_of_genes)
+
         print(population)
-        hrabanie(population, riadky, stlpce, fitness_zoznam)
+        zoznam_objektov[j] = hrabanie(population, riadky, stlpce, fitness_zoznam)
 
     max = fitness_zoznam[0]
     min = fitness_zoznam[0]
@@ -160,6 +248,39 @@ def main():
 
     print(fitness_zoznam)
     print("\nMax. prvok: " + str(max) + " Min. prvok " + str(min) + " primer " + str(sum/pocet_jedincov))
+
+    prvy_rodic = turnaj(fitness_zoznam)
+    print(prvy_rodic)
+    druhy_rodic = turnaj(fitness_zoznam)
+    print(druhy_rodic)
+    print(zoznam_objektov)
+    print(zoznam_objektov.get(prvy_rodic))
+    print(zoznam_objektov.get(druhy_rodic))
+    deti = krizenie(zoznam_objektov.get(prvy_rodic), zoznam_objektov.get(druhy_rodic))
+    print(deti[0])
+    print(deti[1])
+
+    new_population = {}
+    j = 0
+    for i in range(0, int(pocet_jedincov/2)):
+        prvy_rodic = turnaj(fitness_zoznam)
+        druhy_rodic = turnaj(fitness_zoznam)
+        deti = krizenie(zoznam_objektov.get(prvy_rodic), zoznam_objektov.get(druhy_rodic))
+        new_population[j] = deti[0]
+        new_population[j+1] = (deti[1])
+        j += 2
+
+
+    print(new_population.get(0)[1].get_suradnice())
+    hrabanie_2(new_population[0], riadky, stlpce, fitness_zoznam)
+
+    """
+    for i in range(1, pocet_generacii):
+        population
+
+        for j in range(0, pocet_jedincov):
+            zoznam_objektov[j] = hrabanie(population, riadky, stlpce, fitness_zoznam)
+    """
 
 
 if __name__ == "__main__":

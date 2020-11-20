e1ae89148fd1abfe14d700aca6c46936b06748e2
Adamko :) <adam.hrac@gmail.com>
2020-11-21 00:26:09 +0100

    pridanie dalsich komentarov

--- gen_class.py
+++ gen_class.py
@@ -1,13 +1,13 @@
-class Gene:
-    def __init__(self, start, riadky, stlpce, sanca):
-        self.start = start
-        if riadky < start <= stlpce + riadky:
-            self.smer = "Up"
-            self.povodny_smer = "Up"
-            self.x = start - riadky - 1
-            self.y = riadky - 1
+class Gene:                                             # trieda pre objekty typu gen
+    def __init__(self, start, riadky, stlpce, sanca):   # konštruktor
+        self.start = start                              # do startu zapisem start
+        if riadky < start <= stlpce + riadky:           # zistim suradnice
+            self.smer = "Up"                            # nastavim smer
+            self.povodny_smer = "Up"                    # rovnako povodny smer
+            self.x = start - riadky - 1                 # suradnicu x
+            self.y = riadky - 1                         # suradnicu y
         elif riadky + stlpce + riadky < start <= riadky + stlpce + riadky + stlpce:
-            self.smer = "Down"
+            self.smer = "Down"                          # analogicky dalej
             self.povodny_smer = "Down"
             self.x = stlpce - (start - riadky*2 - stlpce)
             self.y = 0
@@ -21,35 +21,35 @@ class Gene:
             self.povodny_smer = "Left"
             self.x = stlpce - 1
             self.y = riadky - (start - riadky - stlpce)
-        self.x_posun = self.x
-        self.y_posun = self.y
+        self.x_posun = self.x                           # x_posun nastavím na x
+        self.y_posun = self.y                           # y_posun nastavim na y
 
-        if sanca > 0.5:
-            self.odbocenie = True
+        if sanca > 0.5:                                 # odbocenie podla vstupnej hodnoty
+            self.odbocenie = True                       # bud na True
         else:
-            self.odbocenie = False
+            self.odbocenie = False                      # alebo False
 
-    def get_smer(self):
+    def get_smer(self):                                 # metoda na hodnotu smeru
         return self.smer
 
-    def get_suradnice(self):
+    def get_suradnice(self):                            # metoda na hodnotu suradnic
         return self.x, self.y
 
-    def get_posun(self):
+    def get_posun(self):                                # metoda na hondotu posunu
         return self.x_posun, self.y_posun
 
-    def set_smer(self, smer):
+    def set_smer(self, smer):                           # metoda na nastavenie smeru
         self.smer = smer
 
-    def set_x_posun(self, hodnota):
+    def set_x_posun(self, hodnota):                     # na nastavenie x_posunu
         self.x_posun = self.x_posun + hodnota
 
-    def set_y_posun(self, hodnota):
+    def set_y_posun(self, hodnota):                     # na nastavenie y_posunu
         self.y_posun = self.y_posun + hodnota
 
-    def reset_posun(self, x, y):
+    def reset_posun(self, x, y):                        # na reset posunu
         self.x_posun = x
         self.y_posun = y
 
-    def get_odbocenie(self):
+    def get_odbocenie(self):                            # na ziskanie odbocenia
         return self.odbocenie
--- naraz.py
+++ naraz.py
@@ -1,20 +1,20 @@
-def naraz(gen, mapa, riadky, stlpce):
-    smer = gen.get_smer()
-    if smer == "Up" or smer == "Down":
-        if smer == "Up":
-            suradnice = gen.get_posun()
-            if gen.get_odbocenie():
+def naraz(gen, mapa, riadky, stlpce):                       # funkcia na prehladavanie pri narazeni
+    smer = gen.get_smer()                                   # zistime smer
+    if smer == "Up" or smer == "Down":                      # ak je hore alebo dole
+        if smer == "Up":                                    # ak je hore
+            suradnice = gen.get_posun()                     # zisti suradnice
+            if gen.get_odbocenie():                         # zisti hodnotu odbocenia
                 if suradnice[0] < (stlpce - 1) and mapa[suradnice[1] + 1][suradnice[0] + 1] == 0:
-                    gen.set_smer("Right")
-                    gen.set_y_posun(1)
-                    return 1
+                    gen.set_smer("Right")                   # skontroluj smer doprava a nastav ho
+                    gen.set_y_posun(1)                      # prestav premennu posun
+                    return 1                                # vrat hodnotu 1
                 if suradnice[0] > 0 and mapa[suradnice[1] + 1][suradnice[0] - 1] == 0:
-                    gen.set_smer("Left")
+                    gen.set_smer("Left")                    # analogicky pre odbocenie do lava
                     gen.set_y_posun(1)
                     return 1
-                if gen.y_posun + 1 == riadky - 1:
-                    return 2
-            else:
+                if gen.y_posun + 1 == riadky - 1:           # ak sme nenasli cestu a sme na kraji
+                    return 2                                # vrat 2
+            else:                                           # to iste iba v opacnom poradi prehladavanie odbocenia
                 if suradnice[0] > 0 and mapa[suradnice[1] + 1][suradnice[0] - 1] == 0:
                     gen.set_smer("Left")
                     gen.set_y_posun(1)
@@ -26,9 +26,9 @@ def naraz(gen, mapa, riadky, stlpce):
                 if gen.y_posun + 1 == riadky - 1:
                     return 2
 
-        else:
-            suradnice = gen.get_posun()
-            if gen.get_odbocenie():
+        else:                                               # ak je smer dole
+            suradnice = gen.get_posun()                     # zisti posun
+            if gen.get_odbocenie():                         # dalej rovnako ako hore
                 if 0 < suradnice[0] and mapa[suradnice[1] - 1][suradnice[0] - 1] == 0:
                     gen.set_smer("Left")
                     gen.set_y_posun(-1)
@@ -37,9 +37,9 @@ def naraz(gen, mapa, riadky, stlpce):
                     gen.set_smer("Right")
                     gen.set_y_posun(-1)
                     return 1
-                if gen.y_posun - 1 == 0:
-                    return 2
-            else:
+                if gen.y_posun - 1 == 0:                    # ak sme nenasli cestu a sme na kraji
+                    return 2                                # vrat 2
+            else:                                           # to iste, len v opacnom poradi
                 if suradnice[0] < (stlpce - 1) and mapa[suradnice[1] - 1][suradnice[0] + 1] == 0:
                     gen.set_smer("Right")
                     gen.set_y_posun(-1)
@@ -48,13 +48,13 @@ def naraz(gen, mapa, riadky, stlpce):
                     gen.set_smer("Left")
                     gen.set_y_posun(-1)
                     return 1
-                if gen.y_posun - 1 == 0:
-                    return 2
-        if suradnice[0] == (stlpce - 1) or suradnice[0] == 0:  # pripad, že je to bod na hranici
-            return 2
+                if gen.y_posun - 1 == 0:                    # ak sme na kraji bez cesty
+                    return 2                                # vrat 2
+        if suradnice[0] == (stlpce - 1) or suradnice[0] == 0:  # pripad, že je to bod na hranici, bez cesty
+            return 2                                        # vrat 2
 
-    else:
-        if smer == "Right":
+    else:                                                   # ak je smer doprava alebo dolava
+        if smer == "Right":                                 # rovnaky postup ako hore
             suradnice = gen.get_posun()
             if gen.get_odbocenie():
                 if 0 < suradnice[1] < (riadky - 1) and mapa[suradnice[1] - 1][suradnice[0] - 1] == 0:
@@ -65,9 +65,9 @@ def naraz(gen, mapa, riadky, stlpce):
                     gen.set_smer("Down")
                     gen.set_x_posun(-1)
                     return 1
-                if gen.x_posun - 1 == 0:
-                    return 2
-            else:
+                if gen.x_posun - 1 == 0:                    # ak sme na kraji
+                    return 2                                # vrat 2
+            else:                                           # opacne poradie
                 if 0 < suradnice[1] < (riadky - 1) and mapa[suradnice[1] + 1][suradnice[0] - 1] == 0:
                     gen.set_smer("Down")
                     gen.set_x_posun(-1)
@@ -76,10 +76,10 @@ def naraz(gen, mapa, riadky, stlpce):
                     gen.set_smer("Up")
                     gen.set_x_posun(-1)
                     return 1
-                if gen.x_posun - 1 == 0:
-                    return 2
+                if gen.x_posun - 1 == 0:                    # ak sme na kraji
+                    return 2                                # vrat 2
 
-        else:
+        else:                                               # ak je smer dolava
             suradnice = gen.get_posun()
             if gen.get_odbocenie():
                 if suradnice[1] < (riadky - 1) and mapa[suradnice[1] + 1][suradnice[0] + 1] == 0:
@@ -90,9 +90,9 @@ def naraz(gen, mapa, riadky, stlpce):
                     gen.set_smer("Up")
                     gen.set_x_posun(1)
                     return 1
-                if gen.x_posun + 1 == stlpce - 1:                      # hranicny stav
-                    return 2
-            else:
+                if gen.x_posun + 1 == stlpce - 1:           # hranicny stav
+                    return 2                                # vrat 2
+            else:                                           # opacne poradie
                 if 0 < suradnice[1] and mapa[suradnice[1] - 1][suradnice[0] + 1] == 0:
                     gen.set_smer("Up")
                     gen.set_x_posun(1)
@@ -101,17 +101,17 @@ def naraz(gen, mapa, riadky, stlpce):
                     gen.set_smer("Down")
                     gen.set_x_posun(1)
                     return 1
-                if gen.x_posun + 1 == stlpce - 1:  # hranicny stav
-                    return 2
+                if gen.x_posun + 1 == stlpce - 1:           # hranicny stav
+                    return 2                                # vrat 2
         if suradnice[1] == (riadky - 1) or suradnice[1] == 0:   # pripad, že je to bod na hranici
-            return 2
+            return 2                                        # vrat 2
 
-    return 3
+    return 3                                                # ak nie je dalsia cesta a bod nie je na hranici, vrat 3
 
 
-def backtracking(mapa, delete, riadky, stlpce):
-    for i in range(0, riadky):
+def backtracking(mapa, delete, riadky, stlpce):             # funkcia na spatne vymazavanie zaseknutych ciest
+    for i in range(0, riadky):                              # prejdi celu mapu
         for j in range(0, stlpce):
-            if mapa[i][j] == delete:
-                mapa[i][j] = 0
-    return mapa
+            if mapa[i][j] == delete:                        # ak sa hodnota v poli rovna hladanej hodnote
+                mapa[i][j] = 0                              # tak ju premaz na 0
+    return mapa                                             # a vrat takto upravenu mapu
